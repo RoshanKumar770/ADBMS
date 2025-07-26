33bd239286c0b32e7acb0a6c01607e830ff88269
RoshanKumar770 <roshankumar81624@gmail.com>
2025-07-26 17:39:08 +0530

    practical_2

---
+++ practical_2.sql
@@ -0,0 +1,9 @@
+INSERT INTO authors (author_id, name, country) VALUES
+(1, 'Ashish', 'India'),
+(2, 'Smaran', 'USA'),
+(3, 'Vaibhav', 'UK');
+
+INSERT INTO books (book_id, title, author_id) VALUES
+(101, 'Data Science Basics', 1),
+(102, 'AI in Education', 2),
+(103, 'SQL Simplified', 1);
