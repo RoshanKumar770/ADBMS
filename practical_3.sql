0a2788d4bfead20156eb8756da860396b7485ab9
RoshanKumar770 <roshankumar81624@gmail.com>
2025-07-26 17:57:37 +0530

    practical_3

--- practical_3.sql
+++ practical_3.sql
@@ -2,4 +2,4 @@ SELECT
     Books.title,
     Authors.name,
     Authors.country
-FROM Books INNER JOIN Authors ON Books.author_id = Authors.author_id;
+FROM Books INNER JOIN Authors ON Books.author_id = Authors.author_id; 
